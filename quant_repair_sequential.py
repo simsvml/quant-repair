fix zero gradient due to clamp

quant_repair_sequential.py
@@ -305,14 +305,17 @@ def run():
     # Optimizer, learning rate schedule, and loss function
     optimizer = torch.optim.AdamW(
         train_module.parameters(),
-        #lr = 1e-6 * math.sqrt(1 + train_layer_index),
-        # Changed after layer 9
-        lr = 1e-6 * math.sqrt(1 + train_layer_index),
+        # Layer 0:
+        #lr = 1.5e-6 * math.sqrt(1 + train_layer_index),
+        # Layer 1:
+        #lr = 1.0e-6 * math.sqrt(1 + train_layer_index),
+        # Layers 2-31:
+        lr = 1.5e-6,
     )
     lr_scheduler = lr_schedulers.get_exponential_schedule(
         optimizer,
         start_factor = 1.0,
-        end_factor = 0.1,
+        end_factor = 0.5,
         num_training_steps = total_epochs * max_steps_per_epoch,
     )
     loss_fn = nn.MSELoss()
@@ -409,6 +412,7 @@ def run():
                     json.dump(metrics, log_file)
                     log_file.write('\n')
                     log_file.flush()
+
 
         train_checkpoint_index += 1
         state_dict = train_module.state_dict()

quant_unpack.py
@@ -5,6 +5,7 @@ import sys
 from typing import Optional, Any, Dict
 import torch
 from torch import Tensor
+from torch.nn import functional as F
 from torchtune.models import convert_weights
 from torchtune.modules import quantized
 from torchtune.utils import gguf_quant
@@ -27,6 +28,7 @@ LAYER_PARAMETERS = {
 def gguf_unpack_tensor(
     tensor: gguf.ReaderTensor,
     output_name: Optional[str] = None,
+    device = None,
 ) -> Dict[str, Tensor]:
     if output_name is None:
         output_name = tensor.name
@@ -44,6 +46,29 @@ def gguf_unpack_tensor(
 
     state_dict = {}
 
+    def conv_qs(x) -> Tensor:
+        x = torch.from_numpy(x)
+        if device is not None:
+            x = x.to(device)
+        x = x.to(quantized.QUANTIZED_DTYPE[quant])
+        latent = quantized.quantized_to_latent(x, quantized.QS_BOUNDS[quant])
+        #y = quantized.latent_to_quantized(latent, quantized.QS_BOUNDS[quant])
+        #print('roundtrip loss: %s' % (F.mse_loss(y, x),))
+        latent = latent.to('cpu')
+        return latent
+
+    def conv_scales(x) -> Tensor:
+        x = torch.from_numpy(x)
+        if device is not None:
+            x = x.to(device)
+        x = x.to(quantized.QUANTIZED_DTYPE[quant])
+        latent = quantized.quantized_to_latent(x, quantized.SCALES_BOUNDS[quant])
+        #y = quantized.latent_to_quantized(latent, quantized.SCALES_BOUNDS[quant])
+        #print('roundtrip loss: %s' % (F.mse_loss(y, x),))
+        latent = latent.to('cpu')
+        return latent
+
+
     if quant in quantized.UNQUANTIZED_TYPES:
         state_dict[output_name] = torch.from_numpy(tensor.data).view(*shape)
     elif quant == GGMLQuantizationType.Q6_K:
@@ -55,8 +80,8 @@ def gguf_unpack_tensor(
             return state_dict
 
         state_dict.update({
-            '%s_quant.k_qs' % output_name: torch.from_numpy(qs),
-            '%s_quant.k_scales' % output_name: torch.from_numpy(scales),
+            '%s_quant.k_qs' % output_name: conv_qs(qs),
+            '%s_quant.k_scales' % output_name: conv_scales(scales),
             '%s_quant.k_d' % output_name: torch.from_numpy(d),
         })
     elif quant == GGMLQuantizationType.Q5_K:
@@ -68,9 +93,9 @@ def gguf_unpack_tensor(
             return state_dict
 
         state_dict.update({
-            '%s_quant.k_qs' % output_name: torch.from_numpy(qs),
-            '%s_quant.k_sc' % output_name: torch.from_numpy(sc),
-            '%s_quant.k_m' % output_name: torch.from_numpy(m),
+            '%s_quant.k_qs' % output_name: conv_qs(qs),
+            '%s_quant.k_sc' % output_name: conv_scales(sc),
+            '%s_quant.k_m' % output_name: conv_scales(m),
             '%s_quant.k_d' % output_name: torch.from_numpy(d),
             '%s_quant.k_dmin' % output_name: torch.from_numpy(dmin),
         })
@@ -83,9 +108,9 @@ def gguf_unpack_tensor(
             return state_dict
 
         state_dict.update({
-            '%s_quant.k_qs' % output_name: torch.from_numpy(qs),
-            '%s_quant.k_sc' % output_name: torch.from_numpy(sc),
-            '%s_quant.k_m' % output_name: torch.from_numpy(m),
+            '%s_quant.k_qs' % output_name: conv_qs(qs),
+            '%s_quant.k_sc' % output_name: conv_scales(sc),
+            '%s_quant.k_m' % output_name: conv_scales(m),
             '%s_quant.k_d' % output_name: torch.from_numpy(d),
             '%s_quant.k_dmin' % output_name: torch.from_numpy(dmin),
         })
@@ -98,8 +123,8 @@ def gguf_unpack_tensor(
             return state_dict
 
         state_dict.update({
-            '%s_quant.k_qs' % output_name: torch.from_numpy(qs),
-            '%s_quant.k_scales' % output_name: torch.from_numpy(scales),
+            '%s_quant.k_qs' % output_name: conv_qs(qs),
+            '%s_quant.k_scales' % output_name: conv_scales(scales),
             '%s_quant.k_d' % output_name: torch.from_numpy(d),
         })
     elif quant == GGMLQuantizationType.Q2_K:
@@ -111,9 +136,9 @@ def gguf_unpack_tensor(
             return state_dict
 
         state_dict.update({
-            '%s_quant.k_qs' % output_name: torch.from_numpy(qs),
-            '%s_quant.k_sc' % output_name: torch.from_numpy(sc),
-            '%s_quant.k_m' % output_name: torch.from_numpy(m),
+            '%s_quant.k_qs' % output_name: conv_qs(qs),
+            '%s_quant.k_sc' % output_name: conv_scales(sc),
+            '%s_quant.k_m' % output_name: conv_scales(m),
             '%s_quant.k_d' % output_name: torch.from_numpy(d),
             '%s_quant.k_dmin' % output_name: torch.from_numpy(dmin),
         })
@@ -129,7 +154,7 @@ def gguf_unpack_tensor(
             # TODO: Convert grids and signs from index to one-hot format
             #'%s_quant.k_grids' % output_name: torch.from_numpy(grids),
             #'%s_quant.k_signs' % output_name: torch.from_numpy(signs),
-            '%s_quant.k_scales' % output_name: torch.from_numpy(scales),
+            '%s_quant.k_scales' % output_name: conv_scales(scales),
             '%s_quant.k_d' % output_name: torch.from_numpy(d),
         })
     else:
@@ -142,6 +167,9 @@ def main():
     assert len(sys.argv) == 3
     gguf_path = sys.argv[1]
     output_dir = sys.argv[2]
+
+    # Use CUDA for converting quantized weights to latent weights.
+    device = 'cuda'
 
     print('load weights from %r' % (gguf_path,))
     reader = GGUFReader(gguf_path)
@@ -181,7 +209,7 @@ def main():
         for name in tensor_names:
             tensor = named_tensor_map[name]
             short_name = '.'.join(name.split('.')[2:])
-            state_dict.update(gguf_unpack_tensor(tensor, short_name))
+            state_dict.update(gguf_unpack_tensor(tensor, short_name, device=device))
         output_path = os.path.join(output_dir, 'layer%d.pt' % layer_index)
         torch.save(state_dict, output_path)
         del state_dict
@@ -190,7 +218,7 @@ def main():
     def save_single_tensor(module, param_name):
         print(module)
         tensor = named_tensor_map['%s.%s' % (module, param_name)]
-        state_dict = gguf_unpack_tensor(tensor, param_name)
+        state_dict = gguf_unpack_tensor(tensor, param_name, device=device)
         output_path = os.path.join(output_dir, '%s.pt' % module)
         torch.save(state_dict, output_path)
         print('saved %s' % output_path)

torchtune/modules/quantized.py
@@ -14,6 +14,58 @@ def round_ste(t: Tensor) -> Tensor:
     """
     return t + (t.round() - t).detach()
 
+def _expand_bounds(bounds):
+    """
+    Given integer bounds, expand them to floating-point bounds such that all
+    values within the floating-point bounds will round to an integer within the
+    original bounds.
+    """
+    min_bound, max_bound = bounds
+    return min_bound - 0.49, max_bound + 0.49
+
+def latent_to_quantized(x, bounds):
+    """
+    Convert latent weights to quantized weights.  The output will consist
+    entirely of integers within the `bounds`.
+    """
+    min_bound, max_bound = _expand_bounds(bounds)
+    # `clamp` has zero derivative outside the bounds, which can cause weights
+    # near the minimum/maximum to get "stuck".  We instead use `sigmoid` to do
+    # the clamping.  It produces a value in the range `(0, 1)` and has nonzero
+    # derivative everywhere.
+    x = x.sigmoid()
+    # Rescale to the expanded bounds.
+    x = x * (max_bound - min_bound) + min_bound
+    # Round, producing an integer within the original bounds.
+    x = round_ste(x)
+    return x
+
+def quantized_to_latent(x, bounds):
+    """
+    Inverse of `latent_to_quantized`.  This function converts integer-valued
+    quantized weights `x` to floating-point values `y` such that
+    `latent_to_quantized(y, bounds)` produces `x`.
+    """
+    assert x.dtype.is_floating_point, 'must cast x to a floating point type first'
+
+    min_bound, max_bound = _expand_bounds(bounds)
+
+    # These are the inverses of the operations in `latent_to_quantized`,
+    # applied in reverse order.
+
+    # Ignore `round_ste`.  For a quantized weight of 3, there are many values
+    # in the range `(2.5, 3.5)` that would round to the desired output.  We
+    # just pick 3.0 for convenience.
+
+    # Rescale from the expanded bounds to `(0, 1)`.
+    x = (x - min_bound) / (max_bound - min_bound)
+
+    # `logit` is the inverse of `sigmoid`.
+    x = x.logit()
+
+    return x
+
+
 QS_BOUNDS = {
         # KSimple
         GGMLQuantizationType.Q3_K: (-4, 3),
@@ -100,9 +152,9 @@ class QuantizedTensor_KSimple(nn.Module):
         #assert not torch.isnan(self.k_qs).any(), 'got nan in INPUT qs'
         #assert not torch.isnan(self.k_scales).any(), 'got nan in INPUT scales'
         #assert not torch.isnan(self.k_d).any(), 'got nan in INPUT d'
-        qs = round_ste(self.k_qs).clamp(*QS_BOUNDS[self.quant])
+        qs = latent_to_quantized(self.k_qs, QS_BOUNDS[self.quant])
         #assert not torch.isnan(qs).any(), 'got nan in qs'
-        scales = round_ste(self.k_scales).clamp(*SCALES_BOUNDS[self.quant])
+        scales = latent_to_quantized(self.k_scales, SCALES_BOUNDS[self.quant])
         #assert not torch.isnan(scales).any(), 'got nan in scales'
         xs = (self.k_d.unsqueeze(1) * scales).unsqueeze(2) * qs
         #assert not torch.isnan(xs).any(), 'got nan in xs'
@@ -149,13 +201,13 @@ class QuantizedTensor_KWithMin(nn.Module):
         #assert not torch.isnan(self.k_m).any(), 'got nan in INPUT m'
         #assert not torch.isnan(self.k_d).any(), 'got nan in INPUT d'
         #assert not torch.isnan(self.k_dmin).any(), 'got nan in INPUT dmin'
-        qs = round_ste(self.k_qs).clamp(*QS_BOUNDS[self.quant])
+        qs = latent_to_quantized(self.k_qs, QS_BOUNDS[self.quant])
         #assert not torch.isnan(qs).any(), 'got nan in qs'
-        sc = round_ste(self.k_sc).clamp(*SCALES_BOUNDS[self.quant])
+        sc = latent_to_quantized(self.k_sc, SCALES_BOUNDS[self.quant])
         #assert not torch.isnan(sc).any(), 'got nan in sc'
         scale = self.k_d.unsqueeze(1) * sc
         #assert not torch.isnan(scale).any(), 'got nan in scale'
-        m = round_ste(self.k_m).clamp(*SCALES_BOUNDS[self.quant])
+        m = latent_to_quantized(self.k_m, SCALES_BOUNDS[self.quant])
         #assert not torch.isnan(m).any(), 'got nan in m'
         minimum = self.k_dmin.unsqueeze(1) * m
         #assert not torch.isnan(minimum).any(), 'got nan in minimum'